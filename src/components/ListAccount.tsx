d3ee9c37d7d6c065e1ee8e5c01d4672852ac9d16
Bradford Lynch <blynch41@gmail.com>
2021-05-30 07:53:59 -0700

    Some token info

--- src/components/ListAccount.tsx
+++ src/components/ListAccount.tsx
@@ -9,18 +9,37 @@ const userAddress = new PublicKey(
   "FdG56qcR42XREhtNz8SQPYRdCVeTrexwskqizkY95P1Z"
 );
 
+function Token(id: number, forSale: boolean, minBid: number) {
+  if (forSale) {
+    return "Token " + id + " is for sale with a minimum bid of ◎" + minBid;
+  } else {
+    return "Token " + id + " is not for sale.";
+  }
+}
+
 export function ListAccount() {
   const { forgeClient } = useWallet();
   const [tokenAccount, setTokenAccount] = useState<any>(undefined);
+  const [tokenState, setTokenState] = useState<any>(undefined);
 
   const accountData = useEffect(() => {
     forgeClient.account.tokenAccount
       .associated(userAddress, forgeAddress)
       .then((account: any) => {
         setTokenAccount(account.authority.toBase58());
+        const aToken = account.ownedTokens[2];
+        const minBidSol = aToken.minBidLamports / 1e9;
+        setTokenState(Token(aToken.id, aToken.forSale, minBidSol));
         console.log(account);
       });
   }, []);
 
-  return <div>{tokenAccount}</div>;
+  return (
+    <div>
+      Wallet: {tokenAccount}
+      <ol>
+        <li>{tokenState}</li>
+      </ol>
+    </div>
+  );
 }
